b21f2aa910c2ca469b2c1c01ad9e70ab80eb1d9d
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-10-28 10:48:58 -0700

    10-28 WarmUp

---
+++ Week_13/10282017/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO top5000 (id, artist, song, year, raw_total, raw_usa, raw_uk, raw_eur, raw_row)
+VALUES 
+(1,"Bing Crosby","White Christmas",1942,39.903,23.929,5.7,2.185,0.54),
+(2,"Bill Haley & his Comets","Rock Around the Clock",1955,36.503,19.961,7.458,5.663,0.76),
+(3,"Celine Dion","My Heart Will Go On",1998,35.405,12.636,8.944,23.701,3.61),
+(4,"Sinead O'Connor","Nothing Compares 2 U",1990,35.135,14.582,10.242,23.747,2.733),
+(5,"The Beatles","Hey Jude",1968,34.442,25.545,16.39,16.541,1.51),
+(6,"Bryan Adams","(Everything I Do) I Do it For You",1991,34.367,16.523,12.368,23.194,3.121),
+(7,"Whitney Houston","I Will Always Love You",1992,34.112,14.873,11.29,20.753,4.46),
+(8,"Pink Floyd","Another Brick in the Wall (part 2)",1980,32.02,13.081,13.339,23.347,1.553),
+(9,"Irene Cara","Flashdance... What a Feeling",1983,31.305,15.18,11.115,17.803,2.686),
+(10,"Elton John","Candle in the Wind '97",1997,31.054,7.412,12.394,20.358,3.189);
