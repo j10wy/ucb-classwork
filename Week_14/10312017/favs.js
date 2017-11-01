753b9427b4095b243c8458f964333ce9164877a8
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-10-31 21:05:41 -0700

    Pages work

--- Week_14/10312017/favs.js
+++ Week_14/10312017/favs.js
@@ -3,12 +3,18 @@ var fs = require("fs");
 
 var server = http.createServer(function (request, response) {
 	var path = request.url;
-
+	console.log(path)
 	switch (path) {
 		case "/":
-		return foods(request, response);
+			return page(request, response, path);
+		case "/frameworks":
+			return page(request, response, path);
+		case "/food":
+			return page(request, response, path);
+		case "/movies":
+			return page(request, response, path);
 		default:
-		return response.end("Hi");
+			return response.end("Hi");
 	}
 });
 
@@ -16,11 +22,17 @@ server.listen(8080, function () {
 	console.log("Listening on PORT 8080");
 });
 
-function foods(request, response) {
-	fs.readFile(`${__dirname}/html/index.html`, function (error, data) {
+function page(request, response, path) {
+	if (path.length === 1) {
+		path = "/index";
+	}
+	fs.readFile(`${__dirname}/html${path}.html`, function (error, data) {
 		if (error) {
 			throw error;
 		}
+		response.writeHead(200, {
+			"Content-Type": "text/html"
+		});
 		response.end(data);
 	});
 }
