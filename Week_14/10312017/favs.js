cfa57da45bc06e596df142214bd1450d5a4962d5
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-10-31 20:36:35 -0700

    basic routing for favs

--- Week_14/10312017/favs.js
+++ Week_14/10312017/favs.js
@@ -1,10 +1,26 @@
 var http = require("http");
 var fs = require("fs");
 
-var server = http.createServer(function(req,res){
-	res.end("Hello");
+var server = http.createServer(function (request, response) {
+	var path = request.url;
+
+	switch (path) {
+		case "/":
+		return foods(request, response);
+		default:
+		return response.end("Hi");
+	}
 });
 
-server.listen(8080, function(){
+server.listen(8080, function () {
 	console.log("Listening on PORT 8080");
 });
+
+function foods(request, response) {
+	fs.readFile(`${__dirname}/html/index.html`, function (error, data) {
+		if (error) {
+			throw error;
+		}
+		response.end(data);
+	});
+}
