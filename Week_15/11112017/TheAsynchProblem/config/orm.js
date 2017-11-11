f0b492dca0a52cb6123f1961874118d5bf066abe
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-11-11 11:38:41 -0800

    Async issues with Party DB

---
+++ Week_15/11112017/TheAsynchProblem/config/orm.js
@@ -0,0 +1,13 @@
+var connection = require("./connection.js");
+
+var orm = {
+  selectWhere: function(tableInput, colToSearch, valOfCol) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+      return result;
+    });
+  }
+};
+
+module.exports = orm;
