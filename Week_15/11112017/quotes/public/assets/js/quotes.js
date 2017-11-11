842ebd6cd0180e6bb3f31f85665fd01a56ef2b7c
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-11-11 10:27:02 -0800

    Final handlebars warmup

---
+++ Week_15/11112017/quotes/public/assets/js/quotes.js
@@ -0,0 +1,15 @@
+// // Make sure we wait to attach our handlers until the DOM is fully loaded.
+// $(function() {
+
+//   // jQuery event handlers should go here.
+//   $.ajax({
+//     type: "POST",
+//     url: '/post',
+//     data: data,
+//     success: success,
+//     dataType: dataType
+//   });
+
+//   $("button#submit").on("click", function)
+
+// });
