08fd7f20a5788e4d28efab3e08672ea2eb0251c4
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-12-19 20:40:14 -0800

    React + Bootstrap

---
+++ Week_21/12292017/hello_bootstrap/src/components/HelloBootstrap.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const helloBootstrap = ()=>(
+<nav className="navbar navbar-default">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span className="sr-only">Toggle navigation</span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+      <a className="navbar-brand" href="#">Brand</a>
+    </div>
+	</div>
+</nav>
+);
+
+export default helloBootstrap;
