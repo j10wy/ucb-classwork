1765360d35cdc1211ecec7aa0b6880cd245b82dc
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-12-19 20:25:35 -0800

    Import React, display custom component

---
+++ Week_21/12292017/HelloDiv/src/components/HelloDiv.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HelloDiv = () => (
+	<div>
+		<h1>Hello, Jeff!</h1>
+		<ul>
+			<li>Pizza</li>
+			<li>Burrito</li>
+			<li>PB&J</li>
+		</ul>
+	</div>
+);
+
+export default HelloDiv;
---
+++ Week_21/12292017/react_practice/src/components/HelloReact.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const HelloReact = () => <div><h1>Hello, World!</h1><p>Hi</p></div>;
+
+export default HelloReact;
