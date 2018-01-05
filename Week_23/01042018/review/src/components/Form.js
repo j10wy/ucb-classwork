38f4c7ac5e751179494b4b32d3e9280ad2f3ddb6
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-01-04 19:15:53 -0800

    React review

---
+++ Week_23/01042018/review/src/components/Form.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  // Setting the initial values of this.state.username and this.state.password
+  state = {
+    username: "jeff",
+    password: "1245"
+  };
+
+  alertUserInfo = () => {
+    alert(`Username: ${this.state.username} | Password: ${this.state.password} `)
+  }
+
+  render() {
+    return (
+      <form>
+        <p>Username: {this.state.username}</p>
+        <p>Password: {this.state.password}</p>
+        <input
+          type="text"
+          placeholder="Username"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+        />
+        <button onClick={this.handleDeIncrement}>Submit</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
